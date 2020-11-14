e374717230189356d5eb4b64f7a3a07ec24759a2
Bartlomiej Janik <yanekk1990@gmail.com>
2020-11-14 18:55:59 +0100

    poprawiona

--- src/main/java/pl/sda/finalapp/app/categories/persistence/CategoryDAO.java
+++ src/main/java/pl/sda/finalapp/app/categories/persistence/CategoryDAO.java
@@ -7,6 +7,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.stream.Collectors;
 

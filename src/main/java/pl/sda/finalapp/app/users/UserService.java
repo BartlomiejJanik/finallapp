88c962611e151db092105d36a4bfea090e244abc
Bartlomiej Janik <yanekk1990@gmail.com>
2020-11-21 16:22:24 +0100

    Logowanie + User Context

--- src/main/java/pl/sda/finalapp/app/users/UserService.java
+++ src/main/java/pl/sda/finalapp/app/users/UserService.java
@@ -1,6 +1,7 @@
 package pl.sda.finalapp.app.users;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -8,13 +9,20 @@ public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private RoleRepository roleRepository;
 
     public void registerUser(UserRegistrationDTO dto) {
         userRepository.findByEMail(dto.geteMail())
                 .ifPresent(e -> {
                     throw new EmailAlreadyExistsException("Email " + dto.geteMail() + " already in use.");
                 });
-        userRepository.save(User.applyDTO(dto));
+        String passwordHash = passwordEncoder.encode(dto.getPassword());
+        User user = User.applyDTO(dto, passwordHash);
+        user.addRole(roleRepository.findByRoleName(Role.USER));
+        userRepository.save(user);
     }
 
 }
